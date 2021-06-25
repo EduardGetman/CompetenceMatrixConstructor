1d77c46d91918858e7085382d33457d139be28c0
Ustis <mihail.2000@bk.ru>
2021-06-26 00:15:45 +0300

    Реализовано GetKnowledgeByCompetenceId в Employee.cs

--- CompetenceMatrix/entity/Employee.cs
+++ CompetenceMatrix/entity/Employee.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using CompetenceMatrix.repository;
 
 namespace CompetenceMatrix.entity
 {
@@ -10,28 +12,37 @@ namespace CompetenceMatrix.entity
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
-        [Required]
-        public String FullName { get; set; }
+
+        [Required] public String FullName { get; set; }
+
         // Я так понял здесь что-то наебнулось так как знаний должнобыть много.
         // Сообщи как закончишь я доделаю метод GetKnowledgeByCompetenceId
         public Knowledge Knowledge { get; set; }
 
         //Возвращает true, если Position не null TODO: Реализовать свойство get
-        public bool HoldPosition { get => throw new NotImplementedException(); }
+        public bool HoldPosition
+        {
+            get => throw new NotImplementedException();
+        }
 
         //TODO: Когда появиться сылка на должность реализовать.
         //Если должность назначена вернуть название, если отсутсвует вернуть "Должность отсутствует"
-        public string PositionName { get => throw new NotImplementedException(); }
-        // Проверяет соотвуетсвует ли сотрудник требованию
-        public bool MeetRequirement(Requirement requirement )
+        public string PositionName
         {
-            Knowledge RequiredKnowledge = GetKnowledgeByCompetenceId( requirement.Competence.Id);
-            return RequiredKnowledge.Level >= requirement.Level;
-        }
-        private Knowledge GetKnowledgeByCompetenceId(int CompetenceId)
-        {
-            throw new NotImplementedException();
+            get => throw new NotImplementedException();
         }
 
+        // Проверяет соотвуетсвует ли сотрудник требованию
+        public bool MeetRequirement(Requirement requirement)
+        {
+            Knowledge RequiredKnowledge = GetKnowledgeByCompetenceId(requirement.Competence.Id);
+            return RequiredKnowledge.Level >= requirement.Level;
+        }
+
+        private Knowledge GetKnowledgeByCompetenceId(int CompetenceId)
+        {
+            CompetenceMatrixContext context = new CompetenceMatrixContext();
+            return context.Knowledges.Where(k => k.Competence.Id == CompetenceId).FirstOrDefault();
+        }
     }
 }
